ba8db64a05f5945ef74278e8508052aca6dafa3d
Mauricio Zaffari <mauricio@papodenerd.net>
2016-08-25 16:02:28 -0300

    Adds match method to determine if a description matches or not

--- dictionary.rb
+++ dictionary.rb
@@ -2,35 +2,66 @@ require 'active_support/inflector'
 require 'fast_stemmer'
 
 class Dictionary
-  attr_accessor :text, :dictionary
+  attr_accessor :text, :stems, :dictionary
 
-  def initialize(text)
+  def initialize(text = "")
     @text = text
-    @dictionary = scores(text)
+    @stems = get_stems(text)
+    @dictionary = get_dictionary
   end
 
-  def stems(text)
-    stop_words = File.readlines('stopwords.txt').map(&:strip)
-
-    ActiveSupport::Inflector.transliterate(text).
-      downcase.
-      to_s.
-      gsub(/[._:;'"`,?|+={}()!@#%^&*<>~\$\-\\\/\[\]\d+]/, ' '). # strip punctuation
-      gsub(/[^\s\p{Alnum}]/,''). # strip accents
-      split(' ').
-      reject { |word| word.size <= 2 || stop_words.include?(word) }.
-      map(&:stem).
-      inject(Hash.new(0)) {|h,i| h[i] += 1; h }.
-      sort_by { |word, occurrences| occurrences }.reverse.to_h
+  def match(description)
+    "#{score(description) * 100 / max_score}% match"
   end
 
-  def scores(text)
-    stems = stems(text)
-    max_stem_count = stems.values.first
-    stems.inject({}) do |a, stem|
-      score = (10.0 * stem.last / max_stem_count).round
-      a.merge!({ stem.first => score }) if score > 0
-      a
+  def add_to_dictionary(description)
+    @stems += get_stems(description)
+    @dictionary = get_dictionary
+  end
+
+  private
+    def get_stems(text)
+      return [] unless text.length > 0
+
+      stop_words = File.readlines('stopwords.txt').map(&:strip)
+
+      ActiveSupport::Inflector.transliterate(text).
+        downcase.
+        to_s.
+        gsub(/[._:;'"`,?|+={}()!@#%^&*<>~\$\-\\\/\[\]\d+]/, ' '). # strip punctuation
+        gsub(/[^\s\p{Alnum}]/,''). # strip accents
+        split(' ').
+        reject { |word| word.size <= 2 || stop_words.include?(word) }.
+        map(&:stem)
     end
-  end
+
+    def get_dictionary
+      return {} unless self.stems.length > 0
+
+      stems = self.stems.
+        inject(Hash.new(0)) {|h,i| h[i] += 1; h }.
+        reject { |word, occurrences| occurrences < 2 }.
+        sort_by { |word, occurrences| occurrences }.reverse.to_h
+
+      max_stem_count = stems.values.first
+      stems.inject({}) do |a, stem|
+        score = (10.0 * stem.last / max_stem_count).round
+        a.merge!({ stem.first => score / 2 }) if score > 1
+        a
+      end
+    end
+
+    def score(description)
+      uniq_stems = get_stems(description).uniq
+      uniq_stems.inject(0) do |a, stem|
+        if dictionary[stem]
+          a += dictionary[stem]
+        end
+        a
+      end
+    end
+
+    def max_score
+      dictionary.values.inject(0) {|sum, i| sum + i }
+    end
 end
